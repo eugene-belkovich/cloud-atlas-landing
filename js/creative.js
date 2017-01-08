68f065d670b7f608c64f109fa042c416daaa8db7
jeka <eugene.belkovich@gmail.com>
2017-01-09 01:58:26 +0300

    change thumbnail photos, add socials, change background, change about

--- js/creative.js
+++ js/creative.js
@@ -1,6 +1,8 @@
 (function ($) {
     "use strict"; // Start of use strict
 
+    $('.carousel').carousel();
+
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
