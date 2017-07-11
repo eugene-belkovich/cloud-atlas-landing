e9535696ae65a7afbacf362ee0264512f41aed79
Eugene Belkovich <ebelkovich@klika-tech.com>
2017-07-12 01:58:52 +0300

    Demo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
+var cool = require('cool-ascii-faces');
 
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/img', express.static(__dirname + '/img'));
@@ -8,8 +9,12 @@ app.use('/js', express.static(__dirname + '/js'));
 app.use('/less', express.static(__dirname + '/less'));
 app.use('/vendor', express.static(__dirname + '/vendor'));
 
-app.get('/*',function(req,res){
+app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
 });
 
 app.listen(3000);
